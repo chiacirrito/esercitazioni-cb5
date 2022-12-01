7e3d97cf86a5d01d1c80bae3d4d1f5c52747d997
Chiara Cirrito <chiara.cirrito@edu.unito.it>
2022-12-01 17:38:13 +0100

    database registi lato server, 01-12-22

---
+++ 01-12-22/public/js/registi.js
@@ -0,0 +1,73 @@
+const urlRegisti = "http://localhost:3000/registi";
+const urlRegista = "http://localhost:3000/regista";
+const createregisti = document.forms.newregisti.elements;
+const submit = createregisti.submitReg;
+
+const POST = async (url, body) => {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams(body),
+  });
+  return res.json;
+};
+
+//FORM SUBMIT
+submit.addEventListener("click", () => {
+  let reg = {
+    nome: createregisti[1].value,
+    cognome: createregisti[2].value,
+  };
+  console.log(reg);
+  POST(urlRegista, reg);
+});
+
+//DELETE
+const DELETE = async (url, id) => {
+  const res = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({ id: id }),
+  });
+  return res.json();
+};
+
+function createCard(registi) {
+  const container = document.querySelector(".registi");
+  for (let reg of registi) {
+    const cardEl = document.createElement("div");
+    const idEl = document.createElement("h3");
+    const imgEl = document.createElement("div");
+    const nomeEl = document.createElement("h2");
+    const cognomeEl = document.createElement("h2");
+    const btnDel = document.createElement("button");
+
+    cardEl.className = "card";
+    idEl.className = "id";
+    imgEl.className = "img";
+    nomeEl.className = "name";
+    cognomeEl.className = "cognome";
+    btnDel.className = "cancella";
+
+    idEl.textContent = reg.id;
+    nomeEl.textContent = "Nome: " + reg.nome;
+    cognomeEl.textContent = "Cognome: " + reg.cognome;
+    btnDel.textContent = "Cancella";
+
+    cardEl.append(idEl, imgEl, nomeEl, cognomeEl, btnDel);
+    container.append(cardEl);
+
+    btnDel.addEventListener("click", () => {
+      DELETE(urlRegista, reg.id);
+      location.reload();
+    });
+  }
+}
+
+fetch(url)
+  .then((res) => res.json())
+  .then((item) => createCard(item));

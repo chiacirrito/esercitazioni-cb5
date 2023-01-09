ec85f4083da382e22e43c11f7031b37d834d4c90
Chiara Cirrito <chiara.cirrito@edu.unito.it>
2023-01-09 17:43:38 +0100

    09-01-23, ToDoList

---
+++ 22-12-22/my-app/src/atoms/text/Text.jsx
@@ -0,0 +1,17 @@
+import "./text.css";
+
+const variantMap = {
+  body: `text-md`,
+  title: `text-xl mb-sm`,
+};
+
+export const Text = (props) => {
+  const { as = "div", children, variant = "body" } = props;
+
+  const Element = as;
+  return (
+    <Element className={`Text ${variantMap[variant] ?? ""}`}>
+      {children}
+    </Element>
+  );
+};
